ff27cd743a983c2bb24142afd76d93b1e2792daa
tylearymf <tylearymf@gmail.com>
2022-11-18 20:41:03 +0800

    Supported Unity2022.1.23f1 for Linux

--- Patcher/Unity/UnityPatchInfos.cs
+++ Patcher/Unity/UnityPatchInfos.cs
@@ -436,9 +436,16 @@ namespace UniHacker
             new()
             {
                 // 2022.1.15
-                Version = "2022",
+                Version = "2022.1.15",
                 LightPattern = ToBytes(ToArray("0F 84 A7 00 00 00 48 8B 35 D8 CE 27"), ToArray("84 36 01 00 00 48 8D B4 24 A8")),
                 DarkPattern = ToBytes(ToArray("E9 A8 00 00 00 00 48 8B 35 D8 CE 27"), ToArray("85 36 01 00 00 48 8D B4 24 A8")),
+            },
+            new()
+            {
+                // 2022.1.23
+                Version = "2022",
+                LightPattern = ToBytes(ToArray("0F 84 A7 00 00 00 48 8B 35 D0 14 29 01"), ToArray("84 C0 0F 84 36 01 00 00 48 8D B4 24 A8")),
+                DarkPattern = ToBytes(ToArray("E9 A8 00 00 00 00 48 8B 35 D0 14 29 01"), ToArray("84 C0 0F 85 36 01 00 00 48 8D B4 24 A8")),
             },
         };
 
